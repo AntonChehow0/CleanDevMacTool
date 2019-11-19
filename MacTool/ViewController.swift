17cac0a461beffd629ed8ae7ed9b28dcfe2ac2b5
Мусонов Виталий <musonov.vitaliy@yandex.ru>
2019-11-19 09:44:20 +0300

    Initial Commit

---
+++ MacTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacTool
+//
+//  Created by В М on 19.11.2019.
+//  Copyright © 2019 В М. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
